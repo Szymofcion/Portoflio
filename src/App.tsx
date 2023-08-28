f94041b2ca45961fbb64581d4d39936825cabd8a
Szymon Dziewa <82220216+Szymofcion@users.noreply.github.com>
2023-08-28 20:35:37 +0200

    add component nav and some structure project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Nav from "./components/nav";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <h1>Portoflio Szymon Dziewa</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/nav.tsx
@@ -0,0 +1,48 @@
+import "./nav.scss";
+import { useState } from "react";
+import { FaBeer } from "@react-icons/all-files/fa/FaBeer";
+const Nav = () => {
+  const [visibleNav, setVisibleNav] = useState<boolean>(false);
+
+  const toggleNav = () => {
+    setVisibleNav(!visibleNav);
+  };
+
+  return (
+    <nav className="nav">
+      <div className="nav__main">
+        <p className="nav__main-title">Szymon Dziewa</p>
+        <input
+          type="checkbox"
+          role="button"
+          aria-label="Display the menu"
+          className="menu"
+          onClick={toggleNav}
+        ></input>
+      </div>
+      <div className={`nav__container ${visibleNav ? "" : "visible"}`}>
+        <div className="nav__container-links">
+          <a className="nav__container-links--link">Home</a>
+          <div className="line"></div>
+          <a className="nav__container-links--link">About</a>
+          <div className="line"></div>
+          <a className="nav__container-links--link">Project</a>
+          <div className="line"></div>
+          <a className="nav__container-links--link">Contact</a>
+          <div className="line"></div>
+        </div>
+        <div className="nav__container-icons">
+          <a className="nav__container-icons--icon" href="">
+            <FaBeer /> Github
+          </a>
+          <div className="line"></div>
+          <a className="nav__container-icons--icon" href="">
+            <FaBeer /> LinkedIn
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;

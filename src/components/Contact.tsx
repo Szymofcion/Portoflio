3d1d93aa1bd3a7bbeb095e58a99ebd2dda428fbe
Szymon Dziewa <82220216+Szymofcion@users.noreply.github.com>
2023-09-27 21:20:30 +0200

    feat: :sparkles: add handleChange function in contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,26 +1,50 @@
 import emailjs from "@emailjs/browser";
 
 import "./style/Contact.scss";
-import { useState } from "react";
+import { SetStateAction, useState } from "react";
 const Contact = () => {
   const [data, setData] = useState({ name: "", email: "", message: "" });
 
-  const saveParams = (e) => {};
+  const templateParams = {
+    name: data.name,
+    email: data.email,
+    message: data.message,
+  };
 
-  // let templateParams = {
-  //   name: data.name,
-  //   email: data.email,
-  //   message: data.message,
-  // };
+  const handleChange = (event: {
+    target: {
+      value: SetStateAction<{ name: string; email: string; message: string }>;
+    };
+  }) => {
+    const value = event.target.value;
+    setData({
+      ...data,
+      [event.target.name]: value,
+      [event.target.email]: value,
+      [event.target.message]: value,
+    });
+    console.log(event.target.value);
+  };
 
   const sendMail = (e: { preventDefault: () => void }) => {
+    console.log(templateParams);
     e.preventDefault();
-    emailjs.sendForm(
-      "service_66hujqe",
-      "template_qm5gxop",
-      e.target,
-      "1ZuleerAVWhMqfbL1"
-    );
+
+    emailjs
+      .send(
+        "service_66hujqe",
+        "template_qm5gxop",
+        templateParams,
+        "1ZuleerAVWhMqfbL1"
+      )
+      .then(
+        (response) => {
+          console.log("SUCCESS!", response.status, response.text);
+        },
+        (err) => {
+          console.log("FAILED...", err);
+        }
+      );
   };
 
   return (
@@ -28,7 +52,14 @@ const Contact = () => {
       <h2>Get In Touch</h2>
       <div className="contact__form-top">
         <label className="contact__form-label">Name:</label>
-        <input type="text" className="contact__form-input" id="name" />
+        <input
+          type="text"
+          className="contact__form-input"
+          id="name"
+          onChange={handleChange}
+          name="name"
+          value={data.name}
+        />
         <label className="contact__form-label">E-mail:</label>
         <input
           type="text"
@@ -36,12 +67,16 @@ const Contact = () => {
           name="email"
           id="emailFrom"
           placeholder="person@example.com"
+          onChange={handleChange}
+          value={data.email}
         />
         <label className="contact__form-label">Message</label>
         <textarea
           className="contact__form-textarea"
           id="msg"
           name="message"
+          value={data.message}
+          onChange={handleChange}
         ></textarea>
         <button type="submit" className="contact__form-btn ">
           SEND IT
